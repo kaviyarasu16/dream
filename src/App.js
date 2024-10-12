77c2e949809b952fe79b599fd18c2b2452ee3de7
Kaviyarasu P <kavi@Kaviyarasus-MacBook-Pro.local>
2024-10-12 12:11:25 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
-import { useAuthenticator } from '@aws-amplify/ui-react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import VideoList from './components/VideoList';
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Authenticator } from '@aws-amplify/ui-react';
 import './index.css';
 import App from './App';
-import '@aws-amplify/ui-react/styles.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>
 );
 
