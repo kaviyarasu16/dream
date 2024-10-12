e3ab17977fca7e6aa6fec71096bd910fc0664397
Kaviyarasu P <kavi@Kaviyarasus-MacBook-Pro.local>
2024-10-12 12:04:43 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import VideoList from './components/VideoList';
