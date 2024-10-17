ecdcb111b849b1ff99f313541cb57ca6f3f05de9
Kaviyarasu P <kavi@Kaviyarasus-MacBook-Pro.local>
2024-10-17 08:20:17 +0530

    new

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,10 @@ const awsmobile = {
   "aws_project_region": "ap-south-1",
   "aws_cognito_region": "ap-south-1",
   "aws_user_pools_id": "ap-south-1_yMfLTJ3KT",
-  "aws_user_pools_web_client_id": "6kdn891eu9i132u90nhj2rkd0s"
-}
+  "aws_user_pools_web_client_id": "6kdn891eu9i132u90nhj2rkd0s",
+  // Add your S3 bucket name and region
+  "aws_user_files_s3_bucket": "kavi-testing", // replace with your bucket name
+  "aws_user_files_s3_bucket_region": "ap-south-1" // region of your bucket
+};
 
 export default awsmobile;
