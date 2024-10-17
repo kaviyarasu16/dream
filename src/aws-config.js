521910f2841757ea8db78ead59a4cfcbd0f3f2ce
Kaviyarasu P <kavi@Kaviyarasus-MacBook-Pro.local>
2024-10-18 05:11:54 +0530

    new

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,8 +1,8 @@
 // src/aws-config.js
 export const awsConfig = {
     region: process.env.REACT_APP_AWS_REGION,
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-    bucket: process.env.REACT_APP_AWS_S3_BUCKET,
+    accessKeyId: process.env.REACT_APP_MY_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_MY_SECRET_ACCESS_KEY,
+    bucket: process.env.REACT_APP_S3_BUCKET_NAME,
   };
   
